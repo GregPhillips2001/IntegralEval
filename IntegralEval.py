9657d8fdcdf47ab8747d286a45d0cdd928bd119e
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2018-12-04 10:48:56 -0500

    Updated from Brython Server: 12/4/2018 10:48:56 AM

--- IntegralEval.py
+++ IntegralEval.py
@@ -1,1 +1,34 @@
+from math import*
+
+f = input('Enter the function: ')
+a = float(input('Enter the left endpoint: '))
+b = float(input('Enter the right endpoint: '))
+n = int(input('Enter the number of subintervals: '))
+
+LRAM = 0.0
+RRAM = 0.0
+MRAM = 0.0
+
+x = a
+h = (b - a)/n
+for i in range (n):
+    x1 = x
+    f1 = eval(f)
+    LRAM = LRAM + f1*h
+    x = x + h
+    x2 = x
+    f2 = eval(f)
+    RRAM = RRAM + f2*h
+    x = (x1 + x2)/2
+    fm = eval(f)
+    MRAM = MRAM + fm*h
+    x = x2
+
+print('LRAM = ' ,LRAM)
+print('MRAM = ' ,MRAM)
+print('RRAM = ' ,RRAM)
+
+T = (LRAM+RRAM)/2
+print("Trapezoidal Rule = ", T)
+print("Simposon's Rule =", (T+2*MRAM)/3)
 
